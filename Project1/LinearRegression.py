7976557506f7cf0b5a25d78bb567e9fc91fb9ce2
njmikkelsen <mikkelsen.nilsjohannes@gmail.com>
2018-09-21 16:56:48 +0200

    Further developement on LinearRegression(). In particular: implemented the Bootstrap and added coefficient confidence intervals for OLS and Ridge.

--- Project1/LinearRegression.py
+++ Project1/LinearRegression.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 import numpy.linalg as la
+import numpy.random as rand
 
 class LinearRegression():
   """
@@ -8,7 +9,7 @@ class LinearRegression():
   
   The two fundamental components of this class is the output vetor Y, and the matrix of input
   vectors X = [X_1 --- X_p]. To investigate the relationship between X and Y, the user will
-  imposes a model (which itself may be a combination of models) using the map F = [f_1 --- f_p].
+  impose a model (which itself may be a combination of models) using the map F = [f_1 --- f_p].
   The user is free to impose as many models as required; in particular, the different models may
   be compared with respect to their MSE scores, R2-scores, confidence intervals, etc. Note that
   each individual component f_i(X) does not necessarily need to use all components of X.
@@ -26,7 +27,7 @@ class LinearRegression():
   Practical issue: In order for the user-defined F(X) map to work with LinearRegression(), it
                    must a) accept a single matrix argument X whose columns represent each input
                    vector, and b) return a design matrix whose columns represent each mapping
-                   f_i(X) i=1,...,l.
+                   f_i(X) for i=1,...,l.
     
   Attributes:
     X_input    | input data (multivariate)
@@ -71,29 +72,31 @@ class LinearRegression():
   # perform regression analysis
   def run_analysis(self, alpha=0):
     for key in self.model.keys():
-      # regression with resampling
-      if self.resampling:
-        if self.technique == "Kfold":
-          l         = 0
-          MSE_cross = 0
-          R2_cross  = 0
-          for k in range(0,len(self.Y_output),self.K):
-            X_test  = self.X_input [k:k+self.K,:]
-            Y_test  = self.Y_output[k:k+self.K,:]
-            X_train = np.concatenate((self.X_input [:k,:],self.X_input [k+self.K:,:]))
-            Y_train = np.concatenate((self.Y_output[:k,:],self.Y_output[k+self.K:,:]))
-            self.model[key].regress(X_train,Y_train,self.method,alpha)
-            self.model[key].compute_R2(X_test,Y_test)
-            MSE_cross += self.model[key].MSE
-            R2_cross  += self.model[key].R2
-            l         += 1.
-          MSE_cross, R2_cross = MSE_cross/l, R2_cross/l
-        elif self.technique == "Bootstrap":
-          pass
       # regression without resampling
-      else:
+      if not self.resampling:
         self.model[key].regress(self.X_input, self.Y_output,self.method,alpha)
         self.model[key].compute_R2(self.X_input,self.Y_output)
+      # regression with resampling
+      if self.resampling:
+        l, MSE_sample, R2_sample = 0, 0, 0
+        if self.technique == "Bootstrap": X_test,Y_test,s = self.X_input,self.Y_output,np.size(self.Y_output)
+        for k in range(self.K):
+          if   self.technique == "Kfold":
+            K       = k*self.K
+            X_test  = self.X_input [K:K+self.K,:]
+            Y_test  = self.Y_output[K:K+self.K,:]
+            X_train = np.concatenate((self.X_input [:K,:],self.X_input [K+self.K:,:]))
+            Y_train = np.concatenate((self.Y_output[:K,:],self.Y_output[K+self.K:,:]))
+          elif self.technique == "Bootstrap":
+            index   = rand.randint(s,size=s)
+            X_train = self.X_input [index,:]
+            Y_train = self.Y_output[index,:]
+          self.model[key].regress(X_train,Y_train,self.method,alpha)
+          self.model[key].compute_R2(X_test,Y_test)
+          MSE_sample += self.model[key].MSE
+          R2_sample  += self.model[key].R2
+          l          += 1.
+        self.model[key].MSE_sample, self.model[key].R2_sample = MSE_sample/l, R2_sample/l
         
 
 # user-defined regression models
@@ -119,15 +122,22 @@ class RegressionModel():
 
   # perform regression
   def regress(self, X_data, Y_data, method, alpha):
-    # Singular Value Decomposition
     design_matrix = self.F(X_data)
-    U,d,VT        = la.svd(design_matrix,full_matrices=False)
+    # Singular Value Decomposition
+    U,d,VT = la.svd(design_matrix,full_matrices=False)
+    V      = VT.T
     # compute regression coefficients
-    if   method == "OLS":   self.beta = VT.T@np.diag(1/d)@U.T@Y_data
-    elif method == "Ridge": self.beta = VT.T@np.diag(d/(d**2+alpha))@U.T@Y_data
+    if method in ["OLS","Ridge"]:
+      sigma2 = np.sum(np.square(Y_data-self(X_data)))/(Y_data.shape[0]-X_data.shape[1])
+      if method == "OLS":
+        self.beta     = V@np.diag(1/d)@U.T@Y_data
+        self.std_beta = np.sqrt(np.diag(V)*(1/d**2)*np.diag(VT)*sigma2)[:,None]
+      if method == "Ridge":
+        self.beta     = V@np.diag(d/(d**2+alpha))@U.T@Y_data
+        self.std_beta = np.sqrt(np.diag(V)*np.power(1+alpha/d**2,-2)*np.diag(VT)*sigma2)[:,None]
     elif method == "Lasso": pass
     self.run = True
-  
+    
   # compute model prediction
   def __call__(self, X_data):
     if self.run: return self.F(X_data)@self.beta
@@ -136,7 +146,7 @@ class RegressionModel():
   # compute Mean Square Error
   def compute_MSE(self, X_data, Y_data):
     if self.run:
-      self.MSE = np.mean(np.power(Y_data-self(X_data).flatten(),2))
+      self.MSE = np.mean(np.square(Y_data-self(X_data).flatten()))
       return self.MSE
     else: return 0
   
@@ -145,8 +155,10 @@ class RegressionModel():
     if self.run:
       y_flat = Y_data.flatten()
       self.compute_MSE(X_data,y_flat)
-      Y_bar   = np.mean(y_flat,dtype=np.float64)
-      self.R2 = 1 - self.MSE/np.mean(np.power(y_flat-Y_bar,2))  # TODO : fix division by zero
+      if len(Y_data) == 1: self.MSE, self.R2 = 0, 1
+      else:
+        Y_bar   = np.mean(y_flat,dtype=np.float64)
+        self.R2 = 1 - self.MSE/np.mean(np.square(y_flat-Y_bar))  # TODO : fix division by zero
       return self.R2
     else: return 0
   
@@ -156,42 +168,48 @@ class RegressionModel():
 
 
 if __name__ == "__main__":
-  import numpy.random as rand
   import matplotlib.pyplot as plt
   # real curve and polynomial approximation maps
-  f  = lambda x: x + np.cos(x)
+  f  = lambda x: np.exp(-x**2)
   def f1(X):
     x = X.flatten()
-    return np.array([np.ones(len(x)),x,x**2,x**3]).T
+    return np.array([np.ones(len(x)),np.exp(-x**2)]).T
   def f2(X):
     x = X.flatten()
-    return np.array([np.ones(len(x)),x,x**2,x**3,x**4,x**5]).T
+    return np.array([np.ones(len(x)),x**2,x**4,x**6]).T
   # data set
-  x        = np.linspace(-5,5,15)
+  x        = np.linspace(-2,2,15)
   x_smooth = np.linspace(x.min(),x.max(),1000)
-  y        = f(x) + 0.5*rand.randn(len(x))
+  y        = f(x) + 0.1*rand.randn(len(x))
   # initialise regression
   LinReg = LinearRegression(x,y)
-  LinReg.add_model(F=f1)
-  LinReg.add_model(F=f2)
+  LinReg.add_model(F=f1,name="f1")
+  LinReg.add_model(F=f2,name="f2")
   # perform analysis
   LinReg.use_method("Ridge")
-  LinReg.setup_resampling("Kfold",K=2)
+  LinReg.run_analysis(alpha=1)
+  F1,F2 = [LinReg.model[key] for key in ["f1","f2"]]
+  LinReg.setup_resampling("Bootstrap",K=100)
   LinReg.run_analysis(alpha=1)
   # plot results
-  plt.plot(x_smooth,f(x_smooth))
-  for key in LinReg.model.keys():
-    A = LinReg.model[key]
-    plt.plot(x_smooth,A(x_smooth))
+  plt.plot(x_smooth,f(x_smooth),label="underlying curve")
+  plt.scatter(x,y,label="data points")
+  plt.plot(x_smooth,F1(x_smooth),label="f1")
+  plt.plot(x_smooth,F2(x_smooth),label="f2")
+  plt.legend(loc="best")
   plt.show()
-  
-      
-    
-  
-  
-  
-  
-  
+  # print results
+  for m,F in zip(["f1","f2"],[F1,F2]):
+    print("\n")
+    print("model {:s}:".format(m))
+    print("  sampled MSE = {:f}".format(LinReg.model[m].MSE_sample))
+    print("  sampled R2  = {:f}".format(LinReg.model[m].R2_sample))
+    print("  beta:")
+    print(F.beta)
+    print("  std(beta):")
+    print(F.std_beta)
+    print("  std(beta)/beta:")
+    print(F.std_beta/F.beta)
   
   
   
